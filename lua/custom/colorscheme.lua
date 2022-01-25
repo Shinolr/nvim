e3eafde8ad01d66d73e92e9105ebb14ee6da9152
shinolr <wyqbeta+github@gmail.com>
2022-01-25 13:24:40 +0800

    feat: setup cmp

--- lua/custom/colorscheme.lua
+++ lua/custom/colorscheme.lua
@@ -1,7 +1,15 @@
-local colorscheme = "doom-one"
-
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
-  return
-end
+-- local colorscheme = "doom-one"
+-- 
+-- local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+-- if not status_ok then
+--   vim.notify("colorscheme " .. colorscheme .. " not found!")
+--   return
+-- end
+vim.cmd [[
+try
+  colorscheme doom-one
+catch /^Vim\%((\a\+)\)\=:E185/
+  colorscheme default
+  set background=dark
+endtry
+]]

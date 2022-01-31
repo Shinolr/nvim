baadae7871b24badc0cb8498e9178466681f9dd3
shinolr <wyqbeta+github@gmail.com>
2022-01-31 17:46:31 +0800

    feat: comment

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -45,6 +45,7 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
   use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
+  use "numToStr/Comment.nvim" -- Easily comment stuff
 
   -- Colorscheme
   use "NTBBloodbath/doom-one.nvim"
@@ -75,8 +76,7 @@ return packer.startup(function(use)
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
   }
-  use "p00f/nvim-ts-rainbow"
-  use "nvim-treesitter/playground"
+  use 'JoosepAlviste/nvim-ts-context-commentstring'
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
